5be49415a957020c9a3eaccaa4361ab698268c0a
artur.rachwal <artur.rachwal@gmail.com>
2017-09-25 15:16:54 +0200

    exercise 8 done - a comment added

--- exercise_8/exercise_8.py
+++ exercise_8/exercise_8.py
@@ -1,6 +1,6 @@
 text_to_format = "{} {} {} {}"
 
-#print(text_to_format.format("a one way")) # yeap, it does not work, we need four arguments here
+#print(text_to_format.format("a one way")) # yeap, it does not work, we need four arguments here (tuple index out of range)
 print(text_to_format.format(1, 2, 3, 4))
 print(text_to_format.format("one", "two", "three", "four"))
 print(text_to_format.format(text_to_format, text_to_format, text_to_format, text_to_format))
