95aa8e6f168756d3f7830f5d39eb4abe92fc57a2
artur.rachwal <artur.rachwal@gmail.com>
2017-09-25 15:11:23 +0200

    exercise 7 done

---
+++ exercise_7/exercise_7.py
@@ -0,0 +1,11 @@
+print("I am formatting some {}".format('stuff'))
+print("aDOT" * 10) # hmmm...??
+
+end1 = "C"
+end2 = "h"
+
+end3 = "F"
+end4 = "g"
+
+print(end1 + end2, end="")
+print(end3 + end4)
